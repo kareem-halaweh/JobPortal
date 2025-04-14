43d3387bf3268e8023f64a5782dc3ef523feeba5
kareem-halaweh <s12327129@stu.najah.edu>
2025-04-14 20:54:06 +0300

    first commit

---
+++ JobPortal/src/app/find-job/find-job.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-find-job',
+  imports: [],
+  templateUrl: './find-job.component.html',
+  styleUrl: './find-job.component.css'
+})
+export class FindJobComponent {
+
+}
