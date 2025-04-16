6656183dfacfdecdd9576093d8a50f6cc21f90b3
Basmala <basmala3bed@gmail.com>
2025-04-16 03:42:31 +0300

    routing, responsiveness, css for seeker layout and profile section finished

--- JobPortal/src/app/seeker-pages/seeker-layout/seeker-layout.component.ts
+++ JobPortal/src/app/seeker-pages/seeker-layout/seeker-layout.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-seeker-layout',
   standalone: true,
-  imports: [RouterLink, RouterOutlet, RouterLinkActive],
+  imports: [RouterLink, RouterOutlet, RouterLinkActive, NgIf, NgClass],
   templateUrl: './seeker-layout.component.html',
   styleUrl: './seeker-layout.component.css'
 })
-export class SeekerLayoutComponent {}
+export class SeekerLayoutComponent {
+
+  sidebarVisible: boolean = false;
+
+  get isMobileScreen(): boolean {
+    return window.innerWidth < 768;
+  }
+
+  toggleSidebar(): void {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+
+
+
+}
--- JobPortal/src/app/seeker-pages/seeker-profile/seeker-profile.component.ts
+++ JobPortal/src/app/seeker-pages/seeker-profile/seeker-profile.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import {NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-seeker-profile',
   imports: [
-    NgIf
+    NgIf,
+    RouterLink
   ],
   templateUrl: './seeker-profile.component.html',
-  styleUrls:['./seeker-profile.component.css']
+  standalone: true,
+  styleUrls: ['./seeker-profile.component.css']
 })
 export class SeekerProfileComponent {
   isEditMode = false;
