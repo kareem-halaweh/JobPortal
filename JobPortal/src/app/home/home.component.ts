c4f2abb6ffd09573fe4d809d377aeaa9a25149e6
Lana-Khdair <s12325211@stu.najah.edu>
2025-04-16 22:05:29 +0300

    carousel in homePage

--- JobPortal/src/app/home/home.component.ts
+++ JobPortal/src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {HeroComponent} from './hero/hero.component';
 import {CategoriesComponent} from './categories/categories.component';
+import {CarouselComponent} from './carousel/carousel.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HeroComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    CarouselComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
