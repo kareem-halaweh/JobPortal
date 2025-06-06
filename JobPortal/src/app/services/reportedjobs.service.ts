93c871272ce7b7128b849131e8d58e1d40331eb3
farahshahin <s12324274@stu.najah.edu>
2025-06-06 14:15:02 +0300

    application

--- JobPortal/src/app/services/reportedjobs.service.ts
+++ JobPortal/src/app/services/reportedjobs.service.ts
@@ -1,85 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {reportedJob} from '../models/reportedjobs.model';
-
 @Injectable({ providedIn: 'root' })
 export class reportejobsService{
-  private reportedJob: reportedJob[] = [
-    {
-      id: 1,
-      job_title: 'Digital Marketing Executive',
-      type: 'Full Time',
-      position: 'nablus',
-      company:'goole',
-      reason:'Fraud',
-      date: '20/04/2024',
-      imageUrl: 'download.jpeg'
-    },{
-      id: 2,
-      job_title: 'Digital Marketing Executive',
-      type: 'part time',
-      position: 'nablus',
-      company:'google',
-      reason:'Fraud',
-      date: '20/05/2024',
-      imageUrl: 'download.jpeg'
-    },{
-      id: 3,
-      job_title: 'Digital Marketing Executive',
-      type: 'part time',
-      position: 'nablus',
-      company:'google',
-      reason:'Fraud',
-      date: '20/05/2024',
-      imageUrl: 'download.jpeg'
-    },{
-      id: 4,
-      job_title: 'Digital Marketing Executive',
-      type: 'part time',
-      position: 'nablus',
-      company:'google',
-      reason:'Fraud',
-      date: '20/05/2024',
-      imageUrl: 'download.jpeg'
-    },{
-      id: 5,
-      job_title: 'Digital Marketing Executive',
-      type: 'part time',
-      position: 'nablus',
-      company:'google',
-      reason:'Fraud',
-      date: '20/05/2024',
-      imageUrl: 'download.jpeg'
-    },{
-      id: 6,
-      job_title: 'Digital Marketing Executive',
-      type: 'Full Time',
-      position: 'nablus',
-      company:'goole',
-      reason:'Fraud',
-      date: '20/04/2024',
-      imageUrl: 'download.jpeg'
-    },{
-      id: 7,
-      job_title: 'Digital Marketing Executive',
-      type: 'Full Time',
-      position: 'nablus',
-      company:'goole',
-      reason:'Fraud',
-      date: '20/04/2024',
-      imageUrl: 'download.jpeg'
-    },{
-      id: 8,
-      job_title: 'Digital Marketing Executive',
-      type: 'Full Time',
-      position: 'nablus',
-      company:'goole',
-      reason:'Fraud',
-      date: '20/04/2024',
-      imageUrl: 'download.jpeg'
-    }];
+  private apiUrl = 'http://localhost/new-laravel/web2-Data/DataSite/public/api';
+  constructor(private http: HttpClient) {}
+  getAllReports(token: string, sort: string) {
+    const headers = {
+      Authorization: `Bearer ${token}`
+    };
 
-getreportedJob():reportedJob []{
-  return this.reportedJob;
-}
+    const params: any = {};
+    if (sort) {
+      params.sort = sort;
+    }
+
+    return this.http.get<reportedJob[]>(
+      'http://localhost:8000/api/reports',
+      { headers, params }
+    );
+  }
+
+
+  approveReport(userId: number, jobId: number, token: string) {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post(`${this.apiUrl}/report-approve/${userId}/${jobId}`, {}, { headers });
+  }
+
+  rejectReport(reportId: number, token: string) {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post(`${this.apiUrl}/report-reject/${reportId}`, {}, { headers });
+  }
 
 }
